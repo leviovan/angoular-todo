50c670d89f5c479dc69afadd2a8caeafcd16220f
Sergey <sereja-galigin@mail.ru>
2024-08-29 15:19:54 +0300

    fix bugs and remarks

--- src/app/component/todo/todo.component.ts
+++ src/app/component/todo/todo.component.ts
@@ -1,66 +1,55 @@
 import { Component } from '@angular/core';
+import { Status, Todo } from 'src/app/types/todo';
 
-
-
-export type statuses='active'| 'completed'|''
-
-export interface Todo{
-  id:number
-  title:string,
-  status:statuses,
-}
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls:['./todo.component.scss']
+  styleUrls: ['./todo.component.scss']
 })
 
-export class Todos  {
+export class Todos {
 
-  private _todos :Todo[]=[]
-  private _id:number=0
-  private _filter:statuses=''
-  public newTodo:string|null=null
+  todos: Todo[] = []
+  id: number = 0
+  filter: Status = ''
+  newTodo: string | null = null
 
-  get todos(){
-    return this._todos
+  setFilter(newFilter: Status) {
+    this.filter = newFilter;
+
   }
 
-  get filter(){
-    return this._filter
-  }
+  addTodo() {
 
-  setFilter(newFilter:statuses) {
-     this._filter=newFilter;
-  }
-
-  addTodo(){
-    if (this.newTodo?.trim()){
+    if (this.newTodo?.trim()) {
       console.log(this.todos);
-      this._id= this._id+1;
-      this._todos = [...this._todos,{id:this._id,status:'active',title:this.newTodo!}]
-      this.newTodo=""
+      this.id = this.id + 1;
+      this.todos = [...this.todos, { id: this.id, status: 'active', title: this.newTodo! }]
+      this.newTodo = ''
     }
   }
 
-  removeTodo(index:number){
-    const indexTodo=this._todos?.map(t=>t.id).indexOf(index)
-    this._todos?.splice(indexTodo,1)
-    this._todos=[...this._todos]
+  removeTodo(index: number) {
+    const indexTodo = this.todos?.map(t => t.id).indexOf(index)
+    this.todos?.splice(indexTodo, 1)
+    this.todos = [...this.todos]
   }
-
- reverseAllTodo(){
-    let tempTodo= this._todos.map((todo)=>{todo.status=todo.status==='active' ? 'completed': "active"; return todo})
-    console.log(tempTodo);
-    
-    this._todos=[...tempTodo]
+  reverseAllTodo() {
+    let tempTodo = this.todos.map((todo) => { todo.status = todo.status === 'active' ? 'completed' : 'active'; return todo })
+    this.todos = [...tempTodo]
   }
-  toggleStatusTodo(index:number){
-    const indexTodo=this._todos?.map(t=>t.id).indexOf(index)
-    this._todos[indexTodo].status= this._todos[indexTodo].status==='active' ? 'completed': "active"
+  toggleStatusTodo(id: number) {
+    const newTodos = this.todos.map((todo) => {
+      todo.id === id && (todo.status = todo.status === 'active' ? 'completed' : 'active')
+      return todo
+    })
+    this.todos = [...newTodos]
   }
-  removeTodos(){
-    this._todos=[]
+  removeTodos() {
+    this.todos = []
+  }
+  isActiveClass(currentFilter: Status) {
+    return !!(this.filter === currentFilter)
   }
 }
--- src/app/pipe/filter-todo.pipe.ts
+++ src/app/pipe/filter-todo.pipe.ts
@@ -1,13 +1,14 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { statuses, Todo } from '../component/todo/todo.component';
+import { Status, Todo } from '../types/todo';
+
 
 @Pipe({
   name: 'filterTodo'
 })
+
 export class FilterTodoPipe implements PipeTransform {
 
-  transform(value: Todo[], filter: statuses): Todo[] {
-
-    return  filter?value.filter(p=>p.status===filter):value
+  transform(value: Todo[], filter: Status): Todo[] {
+    return filter ? value.filter(p => p.status === filter) : value
   }
 }
