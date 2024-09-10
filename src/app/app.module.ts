7064916b2746f019a2e89b0925a213412c79400f
leviovan <sereja-galigin@mail.ru>
2024-09-10 20:16:53 +0300

    refactor(element) Удалил ненужные импорты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
+import { MyTodoLibModule } from 'my-todo-lib';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyTodoLibModule } from 'my-todo-lib'
+
 
 
 
